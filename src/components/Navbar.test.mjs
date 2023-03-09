59c5503f86d777bafa2e78a4d4e652dae5a3bad7
paul-nguyen96 <paul121596@icloud.com>
2023-03-08 18:39:36 -0600

    configure and set up jest testing

---
+++ src/components/Navbar.test.mjs
@@ -0,0 +1,36 @@
+const { render, screen } = require("@testing-library/react");
+import Navbar from "./Navbar";
+
+describe("Navbar", () => {
+  test("renders Navbar component", () => {
+    const handleSearchClick = jest.fn();
+    const searchFilter = true;
+    const navbar = false;
+    const setNavbar = jest.fn();
+    const searchTitle = jest.fn();
+
+    render(
+      <Navbar
+        handleSearchClick={handleSearchClick}
+        searchFilter={searchFilter}
+        navbar={navbar}
+        setNavbar={setNavbar}
+        searchTitle={searchTitle}
+      />
+    );
+
+    const navbarElement = screen.getByRole("navigation");
+    const logoElement = screen.getByText("NeetSeat 🎟");
+    const searchInput = screen.getByPlaceholderText("Search for Events");
+    const searchButton = screen.getByText("🔍 Search");
+    const aboutButton = screen.getByText("ℹ️ About");
+    const contactButton = screen.getByText("🏠 Contact");
+
+    expect(navbarElement).toBeInTheDocument();
+    expect(logoElement).toBeInTheDocument();
+    expect(searchInput).toBeInTheDocument();
+    expect(searchButton).toBeInTheDocument();
+    expect(aboutButton).toBeInTheDocument();
+    expect(contactButton).toBeInTheDocument();
+  });
+});
