7519e59f8f4e3ac62db35387ce8b37b763f24f02
paul-nguyen96 <paul121596@icloud.com>
2023-04-08 14:28:50 -0500

    dancing cartoon

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
 import Navbar from "../components/Navbar";
 import concert from "/concert.jpg";
 import music from "/music.jpg";
+import loading from "/loading.gif";
 
 function About() {
+  //Might want to add lazy loading images using useEffect in the future -- settling for dancing cartoon for now
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div className="about">
       <Navbar />
       <div className="aboutContainer">
         <div className="aboutMain">
-          <div className="concertImages">
-            <img src={music} className="festival" />
-            <img src={concert} className="concert" />
-          </div>
+          {isLoading ? (
+            <div>
+              <img src={loading} style={{ width: "27.5vw", marginLeft:"35px"}} />
+            </div>
+          ) : (
+            <div>
+              <div className="concertImages" onLoad={() => setIsLoading(false)}>
+                <img src={music} className="festival" />
+                <img src={concert} className="concert" />
+              </div>
+            </div>
+          )}
           <div className="aboutDescription">
             <h1>Lorem ipsum</h1>
-            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
-            <p>
+            <h2 style={{color:"gray"}}>Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
+            <p style={{color:"light-gray"}}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
